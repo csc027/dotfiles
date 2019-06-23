980fec598f2083853de2cd21be64320440f9622b
Constantine Chen <csc_dev@protonmail.com>
2019-06-22 17:49:39 -0700

    Added a basic Bourne shell script to setup symlinks for the terminal and vim.

---
+++ setup.sh
@@ -0,0 +1,21 @@
+SCRIPT_LOCATION=$(readlink -f $0);
+SCRIPT_DIRECTORY=$(dirname $SCRIPT_LOCATION);
+
+declare -a SYMLINKS=(".vimrc" ".vim" ".dircolors");
+
+for i in ${SYMLINKS[@]}
+do
+	SOURCE=$SCRIPT_DIRECTORY/$i;
+	DESTINATION=$HOME/$i;
+
+	echo -n "Checking if the symlink at '$DESTINATION' exists... ";
+	if [ ! -e $DESTINATION ]; then
+		echo -e "done.  The symlink does not exist.";
+		echo -n "Creating a symlink at '$DESTINATION'... ";
+		ln -s $SOURCE $DESTINATION;
+		echo "done";
+	else
+		echo "done.";
+		echo "The symlink '$DESTINATION' already exists.";
+	fi
+done

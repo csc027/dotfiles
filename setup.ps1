83ecf6b107a6237d2e8ef6b0028f2124dcb9fa22
Constantine Chen <csc_dev@protonmail.com>
2019-06-22 21:59:18 -0700

    Fixed some bugs in the Powershell setup script.

--- setup.ps1
+++ setup.ps1
@@ -1,15 +1,26 @@
 $Script_Location = $PsScriptRoot;
 $Script_Directory = Split-Path -Path $Script_Location -Parent | Get-Item;
 
-if(([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+if(-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
 	throw New-Object System.Exception "The setup script requires administrator privileges to run.";
 }
 
-$Symlinks = @(".vimrc", ".vim");
+$Items = @(
+	@{
+		"Source" = ".vimrc";
+		"Destination" = ".vimrc";
+	},
+	@{
+		"Source" = ".vim";
+		"Destination" = "vimfiles";
+	}
+) | ForEach-Object {
+	New-Object -TypeName PsObject -Property $_;
+};
 
-foreach($Symlink in $Symlinks) {
-	$Source = Join-Path -Path $Script_Directory -ChildPath $Symlink;
-	$Destination = Join-Path -Path $HOME -ChildPath $Symlink;
+foreach($Item in $Items) {
+	$Source = Join-Path -Path $Script_Directory -ChildPath $Item.Source;
+	$Destination = Join-Path -Path $HOME -ChildPath $Item.Destination;
 
 	Write-Host "Checking if the symlink at '$Destination' exists... " -NoNewLine;
 	if(-not (Test-Path -Path $Destination)) {
@@ -20,7 +31,7 @@ foreach($Symlink in $Symlinks) {
 		} elseif(Test-Path -Path $Source -PathType Leaf) {
 			cmd /c mklink $Destination $Source;
 		} else {
-			throw New-Object System.IO.Exception "The source item $Source does not exist.";
+			throw New-Object System.Exception "The source item $Source does not exist.";
 		}
 		Write-Host "done";
 	} else {

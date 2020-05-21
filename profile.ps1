42fffab38c056622d14c827b2fa11826a077003c
Constantine Chen <csc_dev@protonmail.com>
2020-05-20 22:54:44 -0700

    Added a file check for the console colors file.

--- profile.ps1
+++ profile.ps1
@@ -1,4 +1,14 @@
 $ProfileDirectory = Split-Path -Path $Profile -Parent;
+$SolarizedDefaultFile = switch($HOST.UI.RawUI.BackgroundColor.ToString()) {
+	"White" { "Set-SolarizedLightColorDefaults.ps1" }
+	"Black" { "Set-SolarizedDarkColorDefaults.ps1" }
+	default { return }
+};
+$SolarizedDefaultPath = Join-Path -Path $ProfileDirectory -ChildPath $SolarizedDefaultFile
+if(Test-Path -Path $SolarizedDefaultPath) {
+	. $SolarizedDefaultPath;
+}
+
 $AliasModule = Join-Path -Path $ProfileDirectory -ChildPath "aliases.psm1";
 
 $Modules = @(

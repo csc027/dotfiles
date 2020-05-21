fc5f7809d3a25efd366f76bdc28cea050945bc93
Constantine Chen <csc_dev@protonmail.com>
2020-05-20 22:30:26 -0700

    Updated the profile to load the aliases.

--- profile.ps1
+++ profile.ps1
@@ -1,1 +1,11 @@
-Import-Module -Name "posh-git" -Force;
+$ProfileDirectory = Split-Path -Path $Profile -Parent;
+$AliasModule = Join-Path -Path $ProfileDirectory -ChildPath "aliases.psm1";
+
+$Modules = @(
+	$AliasModule,
+	"posh-git"
+);
+
+foreach($Module in $Modules) {
+	Import-Module -Name $Module -Force;
+}

f182cde0b3d32c56e3f1f83b7db66f3c61cddf7c
Constantine Chen <csc_dev@protonmail.com>
2020-06-05 16:09:29 -0700

    Added git subcommand aliases.

--- aliases.psm1
+++ aliases.psm1
@@ -1,47 +1,67 @@
 function Invoke-GitAdd { git add $args }
 function Invoke-GitBranch { git branch $args }
+function Invoke-GitBranchAll { git branch -a $args }
 function Invoke-GitCheckout { git checkout $args }
+function Invoke-GitCheckoutBranch { git checkout -b $args }
 function Invoke-GitCherryPick { git cherry-pick $args }
 function Invoke-GitCommit { git commit -m $args }
 function Invoke-GitDiff { git diff $args }
 function Invoke-GitFetch { git fetch $args }
+function Invoke-GitFetchAll { git fetch --all $args }
+function Invoke-GitFetchPrune { git fetch --prune $args }
+function Invoke-GitFetchAllPrune { git fetch --all --prune $args }
 function Invoke-GitGrep { git grep $args }
 function Invoke-GitInit { git init $args }
-function Invoke-GitLog { git log --decorate --oneline --graph --color $args }
+function Invoke-GitLog { git log --decorate --graph --color $args }
+function Invoke-GitLogOneline { git log --decorate --oneline --graph --color $args }
 function Invoke-GitMerge { git merge $args }
 function Invoke-GitMv { git mv $args }
 function Invoke-GitPull { git pull $args }
 function Invoke-GitPush { git push $args }
 function Invoke-GitRebase { git rebase $args }
-function Invoke-GitRemote { git remote $args }
+function Invoke-GitRebaseInteractive { git rebase --interactive $args }
+function Invoke-GitRemote { git remote -v $args }
 function Invoke-GitReset { git reset $args }
 function Invoke-GitShow { git show $args }
 function Invoke-GitStash { git stash $args }
+function Invoke-GitStashApply { git stash apply $args }
+function Invoke-GitStashList { git stash list $args }
+function Invoke-GitStashPop { git stash pop $args }
 function Invoke-GitStatus { git status $args }
 function Invoke-GitTag { git tag $args }
 
 $Aliases = @{
-	"ga" = "Invoke-GitAdd";
-	"gb" = "Invoke-GitBranch";
-	"gk" = "Invoke-GitCheckout";
-	"gq" = "Invoke-GitCherryPick";
-	"gc" = "Invoke-GitCommit";
-	"gd" = "Invoke-GitDiff";
-	"gf" = "Invoke-GitFetch";
-	"gg" = "Invoke-GitGrep";
-	"gi" = "Invoke-GitInit";
-	"gl" = "Invoke-GitLog";
-	"gm" = "Invoke-GitMerge";
-	"gv" = "Invoke-GitMv";
-	"gu" = "Invoke-GitPull";
-	"gp" = "Invoke-GitPush";
-	"ge" = "Invoke-GitRebase";
-	"go" = "Invoke-GitRemote";
-	"gr" = "Invoke-GitReset";
-	"gw" = "Invoke-GitShow";
-	"gh" = "Invoke-GitStash";
-	"gs" = "Invoke-GitStatus";
-	"gt" = "Invoke-GitTag";
+	"ga"   = "Invoke-GitAdd";
+	"gb"   = "Invoke-GitBranch";
+	"gba"  = "Invoke-GitBranchAll";
+	"gk"   = "Invoke-GitCheckout";
+	"gkb"  = "Invoke-GitCheckoutBranch";
+	"gq"   = "Invoke-GitCherryPick";
+	"gc"   = "Invoke-GitCommit";
+	"gd"   = "Invoke-GitDiff";
+	"gf"   = "Invoke-GitFetch";
+	"gfa"  = "Invoke-GitFetchAll";
+	"gfp"  = "Invoke-GitFetchPrune";
+	"gfap" = "Invoke-GitFetchAllPrune";
+	"gg"   = "Invoke-GitGrep";
+	"gi"   = "Invoke-GitInit";
+	"gl"   = "Invoke-GitLog";
+	"glo"  = "Invoke-GitLogOneline";
+	"gm"   = "Invoke-GitMerge";
+	"gv"   = "Invoke-GitMv";
+	"gu"   = "Invoke-GitPull";
+	"gp"   = "Invoke-GitPush";
+	"ge"   = "Invoke-GitRebase";
+	"gei"  = "Invoke-GitRebaseInteractive";
+	"go"   = "Invoke-GitRemote";
+	"gr"   = "Invoke-GitReset";
+	"gw"   = "Invoke-GitShow";
+	"gh"   = "Invoke-GitStash";
+	"gha"  = "Invoke-GitStashApply";
+	"ghl"  = "Invoke-GitStashList";
+	"ghp"  = "Invoke-GitStashPop";
+	"gs"   = "Invoke-GitStatus";
+	"gt"   = "Invoke-GitTag";
 };
 
 foreach($Key in $Aliases.Keys) {

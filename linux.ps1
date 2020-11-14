ecefa0cb25300152968518a202898c39788db52e
Constantine Chen <csc_dev@protonmail.com>
2020-11-13 19:21:36 -0800

    Added OS specific scripts to be loaded by the PowerShell profile.

---
+++ linux.ps1
@@ -0,0 +1,31 @@
+$global:__aliases = @{
+	ac    = "Add-Content";
+	cat   = "Get-Content";
+	cd    = "Set-Location";
+	clear = "Clear-Host";
+	cp    = "Copy-Item";
+	cpp   = "Copy-ItemProperty";
+	diff  = "Compare-Object";
+	echo  = "Write-Output";
+	fc    = "Format-Custom";
+	kill  = "Stop-Process";
+	ls    = "Get-ChildItem";
+	man   = "help";
+	mount = "New-PSDrive";
+	mv    = "Move-Item";
+	ps    = "Get-Process";
+	pwd   = "Get-Location";
+	ri    = "Remove-Item";
+	rm    = "Remove-Item";
+	rmdir = "Remove-Item";
+	sleep = "Start-Sleep";
+	sort  = "Sort-Object";
+	tee   = "Tee-Object";
+	type  = "Get-Content";
+	write = "Write-Output";
+};
+
+$global:__aliases.Keys | ForEach-Object { 
+	Write-Verbose "Adding $_";
+	Set-Alias -Name $_ -Value $global:__aliases[$_] -Scope Global;
+}

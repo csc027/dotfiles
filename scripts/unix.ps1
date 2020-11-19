57b8a933e7338a101952d14d68731b9b3e5cfa63
Constantine Chen <csc_dev@protonmail.com>
2020-11-18 21:26:38 -0800

    Updated the PowerShell scripts to follow the PsScriptAnalyzerSettings file.

--- scripts/unix.ps1
+++ scripts/unix.ps1
@@ -1,29 +1,33 @@
+[Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidGlobalVars', '', Justification = 'This script requires the use of global variables.')]
+param(
+)
+
 $global:__aliases = @{
-	ac    = "Add-Content";
-	cat   = "Get-Content";
-	cd    = "Set-Location";
-	clear = "Clear-Host";
-	cp    = "Copy-Item";
-	cpp   = "Copy-ItemProperty";
-	diff  = "Compare-Object";
-	echo  = "Write-Output";
-	kill  = "Stop-Process";
-	ls    = "Get-ChildItem";
-	man   = "Get-Help";
-	mount = "New-PSDrive";
-	mv    = "Move-Item";
-	ps    = "Get-Process";
-	pwd   = "Get-Location";
-	rm    = "Remove-Item";
-	rmdir = "Remove-Item";
-	sleep = "Start-Sleep";
-	sort  = "Sort-Object";
-	tee   = "Tee-Object";
-	type  = "Get-Content";
-	write = "Write-Output";
+	ac = 'Add-Content';
+	cat = 'Get-Content';
+	cd = 'Set-Location';
+	clear = 'Clear-Host';
+	cp = 'Copy-Item';
+	cpp = 'Copy-ItemProperty';
+	diff = 'Compare-Object';
+	echo = 'Write-Output';
+	kill = 'Stop-Process';
+	ls = 'Get-ChildItem';
+	man = 'Get-Help';
+	mount = 'New-PSDrive';
+	mv = 'Move-Item';
+	ps = 'Get-Process';
+	pwd = 'Get-Location';
+	rm = 'Remove-Item';
+	rmdir = 'Remove-Item';
+	sleep = 'Start-Sleep';
+	sort = 'Sort-Object';
+	tee = 'Tee-Object';
+	type = 'Get-Content';
+	write = 'Write-Output';
 };
 
-$global:__aliases.Keys | ForEach-Object { 
+$global:__aliases.Keys | ForEach-Object {
 	Write-Verbose "Adding $_";
 	Set-Alias -Name $_ -Value $global:__aliases[$_] -Option AllScope -Scope Global;
 }

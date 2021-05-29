5c8583fb7ba2e6cb7769495dc1296fd6631cf2c2
Constantine Chen <csc_dev@protonmail.com>
2021-05-29 13:01:41 -0700

    Fixed a bug where the status would not correctly display the counts for renamed and copied files.  Simplified the prompt code.  Renamed some variables in the prompt script.

--- .git-prompt.bash
+++ .git-prompt.bash
@@ -52,7 +52,7 @@ if [ "${color_prompt}" == "yes" ]; then
 fi
 
 git_bash_prompt() {
-	local -i ahead=0 behind=0 deleted=0 modified=0 added=0 staged_deleted=0 staged_modified=0 untracked=0
+	local -i ahead=0 behind=0 index_added=0 index_deleted=0 index_modified=0 index_unmerged=0 worktree_added=0 worktree_deleted=0 worktree_modified=0 worktree_unmerged=0 untracked=0
 	local branch="" error="" remote="" line
 
 	while IFS= read -r line ; do
@@ -74,45 +74,45 @@ git_bash_prompt() {
 			if [[ $remote =~ .*\[.*behind[[:blank:]]+([0-9]+).*\] ]]; then behind=$((${BASH_REMATCH[1]})); fi
 		elif [[ "${x}${y}" = "??" ]]; then ((untracked++))
 		else
-			if [[ "${x}" = "A" ]]; then ((added++)); fi
-			if [[ "${x}" = "D" ]]; then ((staged_deleted++)); fi
-			if [[ "${x}" = "M" ]]; then ((staged_modified++)); fi
-			if [[ "${y}" = "A" ]]; then ((untracked++)); fi
-			if [[ "${y}" = "D" ]]; then ((deleted++)); fi
-			if [[ "${y}" = "M" ]]; then ((modified++)); fi
+			if [[ "${x}" = "A" ]]; then ((index_added++)); fi
+			if [[ "${x}" = "D" ]]; then ((index_deleted++)); fi
+			if [[ "${x}" = "M" ]] || [[ "${x}" = "R" ]] || [[ "${x}" = "C" ]]; then ((index_modified++)); fi
+			if [[ "${x}" = "U" ]]; then ((index_unmerged++)); fi
+			if [[ "${y}" = "A" ]]; then ((worktree_added++)); fi
+			if [[ "${y}" = "D" ]]; then ((worktree_deleted++)); fi
+			if [[ "${y}" = "M" ]]; then ((worktree_modified++)); fi
+			if [[ "${y}" = "U" ]]; then ((worktree_unmerged++)); fi
 		fi
 	done < <(LC_ALL=C git status --porcelain --branch 2>/dev/null || echo -e "xx $?")
 
-	local vcstate staged_state delimiter_state local_state working_state
+	local vcstate index_state delimiter_state worktree_state working_state
 
 	if [[ -n "${error}" ]]; then
 		vcstate=" ${GIT_DELIMITER_COLOR}[${GIT_ERROR_COLOR}${error}${GIT_DELIMITER_COLOR}]"
-	else
+	elif [[ -n "$branch" ]]; then
 		if ((ahead == 0 && behind == 0)); then status_state="${GIT_CLEAN_STATUS_COLOR}${branch} ${GIT_CLEAN_INDICATOR}"
 		elif ((ahead > 0 && behind > 0)); then status_state="${GIT_AHEAD_BEHIND_COLOR}${branch} ${GIT_AHEAD_INDICATOR}${ahead} ${GIT_BEHIND_INDICATOR}${behind}"
 		elif ((ahead > 0)); then status_state="${GIT_AHEAD_COLOR}${branch} ${GIT_AHEAD_INDICATOR}${ahead}"
 		elif ((behind > 0)); then status_state="${GIT_BEHIND_COLOR}${branch} ${GIT_BEHIND_INDICATOR}${behind}"
 		fi
 
-		if ((added > 0 || staged_modified > 0 || staged_deleted > 0)); then
-			staged_state=" ${GIT_STAGED_COLOR}${GIT_ADDED_INDICATOR}${added} ${GIT_MODIFIED_INDICATOR}${staged_modified} ${GIT_DELETED_INDICATOR}${staged_deleted}"
+		if ((index_added > 0 || index_modified > 0 || index_deleted > 0)); then
+			index_state=" ${GIT_STAGED_COLOR}${GIT_ADDED_INDICATOR}${index_added} ${GIT_MODIFIED_INDICATOR}${index_modified} ${GIT_DELETED_INDICATOR}${index_deleted}"
 		fi
-		if (( (added > 0 || staged_modified > 0 || staged_deleted > 0) && (untracked > 0 || modified > 0 || deleted > 0) )); then
+		if (( (index_added > 0 || index_modified > 0 || index_deleted > 0) && (worktree_added > 0 || worktree_modified > 0 || worktree_deleted > 0) )); then
 			delimiter_state=" ${GIT_DELIMITER_COLOR}|"
 		fi
-		if ((untracked > 0 || modified > 0 || deleted > 0)); then
-			local_state=" ${GIT_UNSTAGED_COLOR}${GIT_ADDED_INDICATOR}${untracked} ${GIT_MODIFIED_INDICATOR}${modified} ${GIT_DELETED_INDICATOR}${deleted}"
+		if ((worktree_added > 0 || worktree_modified > 0 || worktree_deleted > 0)); then
+			worktree_state=" ${GIT_UNSTAGED_COLOR}${GIT_ADDED_INDICATOR}${worktree_added} ${GIT_MODIFIED_INDICATOR}${worktree_modified} ${GIT_DELETED_INDICATOR}${worktree_deleted}"
 		fi
 
-		if ((untracked > 0 || modified > 0 || deleted > 0)); then
+		if ((worktree_added > 0 || worktree_modified > 0 || worktree_deleted > 0)); then
 			working_state=" ${GIT_UNSTAGED_COLOR}${GIT_UNSTAGED_INDICATOR}"
-		elif ((added > 0 || staged_modified > 0 || staged_deleted)); then
+		elif ((index_added > 0 || index_modified > 0 || index_deleted)); then
 			working_state=" ${GIT_UNCOMMITTED_COLOR}${GIT_UNCOMMITTED_INDICATOR}"
 		fi
 
-		if [[ -n "$branch" ]]; then
-			vcstate=" ${GIT_DELIMITER_COLOR}[${status_state}${staged_state}${delimiter_state}${local_state}${working_state}${GIT_DELIMITER_COLOR}]"
-		fi
+		vcstate=" ${GIT_DELIMITER_COLOR}[${status_state}${index_state}${delimiter_state}${worktree_state}${working_state}${GIT_DELIMITER_COLOR}]"
 	fi
 
 	echo -e "${vcstate}"

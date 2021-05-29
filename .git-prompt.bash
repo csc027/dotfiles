9903944aee5010848301cf192c2df1afc623d9f4
Constantine Chen <csc_dev@protonmail.com>
2021-05-28 18:08:16 -0700

    Added an extra indicator for working state.

--- .git-prompt.bash
+++ .git-prompt.bash
@@ -33,20 +33,22 @@
 GIT_ADDED_INDICATOR="+"
 GIT_DELETED_INDICATOR="-"
 GIT_MODIFIED_INDICATOR="~"
-GIT_UNTRACKED_INDICATOR="!"
+GIT_UNCOMMITTED_INDICATOR="~"
+GIT_UNSTAGED_INDICATOR="!"
 GIT_CLEAN_INDICATOR="≡"
 GIT_AHEAD_INDICATOR="↑"
 GIT_BEHIND_INDICATOR="↓"
 if [ "${color_prompt}" == "yes" ]; then
+	GIT_AHEAD_BEHIND_COLOR="\033[0;93m"
 	GIT_AHEAD_COLOR="\033[0;92m"
 	GIT_BEHIND_COLOR="\033[0;31m"
-	GIT_AHEAD_BEHIND_COLOR="\033[0;93m"
-	GIT_STAGED_COLOR="\033[0;32m"
-	GIT_UNSTAGED_COLOR="\033[0;31m"
-	GIT_DELIMITER_COLOR="\033[0;93m"
 	GIT_CLEAN_STATUS_COLOR="\033[0;96m"
+	GIT_DELIMITER_COLOR="\033[0;93m"
 	GIT_ERROR_COLOR="\033[0;31m"
 	GIT_RESET_COLOR="\033[0m"
+	GIT_STAGED_COLOR="\033[0;32m"
+	GIT_UNSTAGED_COLOR="\033[0;31m"
+	GIT_UNCOMMITTED_COLOR="\033[0;96m"
 fi
 
 git_bash_prompt() {
@@ -142,18 +144,14 @@ git_bash_prompt() {
     unset _reset_state
     unset _git_state
 
-    local vcstate staged_state delimiter_state local_state
+    local vcstate staged_state delimiter_state local_state working_state
     if [[ -n "${error}" ]]; then
         vcstate="${GIT_DELIMITER_COLOR}[${GIT_ERROR_COLOR}${error}${GIT_DELIMITER_COLOR}]"
     else
-		if ((ahead == 0 && behind == 0)); then
-			status_state="${GIT_CLEAN_STATUS_COLOR}${branch} ${GIT_CLEAN_INDICATOR}"
-		elif ((ahead > 0 && behind > 0)); then
-			status_state="${GIT_AHEAD_BEHIND_COLOR}${branch} ${GIT_AHEAD_INDICATOR}${ahead} ${GIT_BEHIND_INDICATOR}${behind}"
-		elif ((ahead > 0)); then
-			status_state="${GIT_AHEAD_COLOR}${branch} ${GIT_AHEAD_INDICATOR}${ahead}"
-		elif ((behind > 0)); then
-			status_state="${GIT_BEHIND_COLOR}${branch} ${GIT_BEHIND_INDICATOR}${behind}"
+		if ((ahead == 0 && behind == 0)); then status_state="${GIT_CLEAN_STATUS_COLOR}${branch} ${GIT_CLEAN_INDICATOR}"
+		elif ((ahead > 0 && behind > 0)); then status_state="${GIT_AHEAD_BEHIND_COLOR}${branch} ${GIT_AHEAD_INDICATOR}${ahead} ${GIT_BEHIND_INDICATOR}${behind}"
+		elif ((ahead > 0)); then status_state="${GIT_AHEAD_COLOR}${branch} ${GIT_AHEAD_INDICATOR}${ahead}"
+		elif ((behind > 0)); then status_state="${GIT_BEHIND_COLOR}${branch} ${GIT_BEHIND_INDICATOR}${behind}"
 		fi
 		if ((added > 0 || staged_modified > 0 || staged_deleted > 0)); then
 			staged_state=" ${GIT_STAGED_COLOR}${GIT_ADDED_INDICATOR}${added} ${GIT_MODIFIED_INDICATOR}${staged_modified} ${GIT_DELETED_INDICATOR}${staged_deleted}"
@@ -161,11 +159,16 @@ git_bash_prompt() {
 		if (( (added > 0 || staged_modified > 0 || staged_deleted > 0) && (untracked > 0 || modified > 0 || deleted > 0) )); then
 			delimiter_state=" ${GIT_DELIMITER_COLOR}|"
 		fi
-		if ((untracked > 0)) || ((modified > 0)) || ((deleted > 0)); then
+		if ((untracked > 0 || modified > 0 || deleted > 0)); then
 			local_state=" ${GIT_UNSTAGED_COLOR}${GIT_ADDED_INDICATOR}${untracked} ${GIT_MODIFIED_INDICATOR}${modified} ${GIT_DELETED_INDICATOR}${deleted}"
 		fi
+		if ((untracked > 0 || modified > 0 || deleted > 0)); then
+			working_state=" ${GIT_UNSTAGED_COLOR}${GIT_UNSTAGED_INDICATOR}"
+		elif ((added > 0 || staged_modified > 0 || staged_deleted)); then
+			working_state=" ${GIT_UNCOMMITTED_COLOR}${GIT_UNCOMMITTED_INDICATOR}"
+		fi
 		if [[ -n "$branch" ]]; then
-			vcstate="${GIT_DELIMITER_COLOR}[${status_state}${staged_state}${delimiter_state}${local_state}${GIT_DELIMITER_COLOR}] "
+			vcstate="${GIT_DELIMITER_COLOR}[${status_state}${staged_state}${delimiter_state}${local_state}${working_state}${GIT_DELIMITER_COLOR}] "
 		fi
     fi
 

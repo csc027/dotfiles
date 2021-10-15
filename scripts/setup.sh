5b40000f49a9af8c207b5948012159c12db88f52
Constantine Chen <csc_dev@protonmail.com>
2021-10-14 21:11:50 -0700

    Fixed a bug where the symlink wouldn't be created if any parent directories were missing.

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -31,6 +31,7 @@ do
 	if [ ! -e $DESTINATION ]; then
 		echo -e "done.  The symlink does not exist.";
 		echo -n "Creating a symlink at '$DESTINATION'... ";
+		mkdir --parents $DESTINATION;
 		ln -s $SOURCE $DESTINATION;
 		echo "done";
 	elif { [ "$1" = "-f" ] || [ "$1" = "--force" ]; } && [ ! -L $DESTINATION ]; then

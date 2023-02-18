822a4c763854fc50940a910c18d4cd9fb42491c4
Constantine Chen <csc_dev@protonmail.com>
2023-02-17 17:48:42 -0800

    Moved to lualine and nvim-solarized

---
+++ .vim/lua/statusline.lua
@@ -0,0 +1,16 @@
+require('lualine').setup {
+	sections = {
+		lualine_a = { 'mode' },
+		lualine_b = { { 'filename', path = 3 } },
+		lualine_c = { 'diagnostics' },
+		lualine_x = { 'fileformat', 'encoding', 'filetype' },
+		lualine_y = { 'progress' },
+		lualine_z = { 'location' }
+	},
+	options = {
+		component_separators = '|',
+		icons_enabled = false,
+		section_separators = '',
+		theme = 'solarized_dark'
+	}
+}
--- settings/init.lua
+++ settings/init.lua
@@ -3,6 +3,7 @@ require('compare')
 require('complete')
 require('general')
 require('netrw')
+require('statusline')
 require('treesitter')
 require('window')
 
@@ -11,11 +12,8 @@ vim.cmd 'syntax on'
 vim.cmd 'filetype plugin indent on'
 
 -- color settings
-vim.g.solarized_extra_groups = 1
-vim.g.solarized_old_cursor_style = 1
 vim.g.solarized_italics = 0
-vim.g.lightline = { colorscheme = 'solarized', active = { left = { { 'mode', 'paste' }, { 'readonly', 'absolutepath', 'modified' } } } }
-vim.cmd 'colorscheme solarized8_flat'
+vim.cmd 'colorscheme solarized'
 
 if vim.fn.has('termguicolors') then
 	vim.opt.termguicolors = true

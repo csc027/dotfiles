dbd284b03e184ab327a0b25c65e1e9d4b58459f4
Constantine Chen <csc_dev@protonmail.com>
2023-04-10 18:29:05 -0700

    Condensed some settings in the complete.lua file.

--- .vim/lua/complete.lua
+++ .vim/lua/complete.lua
@@ -36,9 +36,7 @@ require('lspconfig')['omnisharp'].setup {
 	on_attach = on_attach,
 	flags = { debounce_text_changes = 150 },
 	cmd = { },
-	handlers = {
-		["textDocument/definition"] = require('omnisharp_extended').handler
-	},
+	handlers = { ["textDocument/definition"] = require('omnisharp_extended').handler },
 	enable_editorconfig_support = true,
 	enable_ms_build_load_projects_on_demand = false,
 	enable_roslyn_analyzers = true,

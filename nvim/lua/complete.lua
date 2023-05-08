c43547d64243ebd82613259ac82c04ec64cd7d06
Constantine Chen <csc_dev@protonmail.com>
2023-05-08 21:19:43 +0000

    Added signature help completion key mapping to insert mode.

--- nvim/lua/complete.lua
+++ nvim/lua/complete.lua
@@ -20,6 +20,7 @@ local on_attach = function(client, bufnr)
 	-- Mappings.
 	-- See `:help vim.lsp.*` for documentation on any of the below functions
 	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set('i', '<C-k>', vim.lsp.buf.signature_help, bufopts)
 	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
 	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
 	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)

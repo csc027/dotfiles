29dcc69f732874af8aa1c1d0d0edd77b621da0c4
Constantine Chen <csc_dev@protonmail.com>
2025-09-21 06:13:39 -0700

    Added razor to treesitter config.

--- nvim/lua/treesitter.lua
+++ nvim/lua/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup {
 	-- A list of parser names, or 'all' (the four listed parsers should always be installed)
-	ensure_installed = { 'c_sharp', 'diff', 'json', 'lua', 'markdown', 'markdown_inline', 'powershell', 'vim', 'vimdoc' },
+	ensure_installed = { 'c_sharp', 'diff', 'json', 'lua', 'markdown', 'markdown_inline', 'powershell', 'razor', 'vim', 'vimdoc' },
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
